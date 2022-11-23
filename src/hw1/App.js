83c654069c5a22aa00698497102b211075a53d40
Phamhuong218 <pt.huong2706@gmail.com>
2022-11-23 10:11:10 +0700

    fix bug for color box hw

--- src/hw1/App.js
+++ src/hw1/App.js
@@ -2,25 +2,27 @@ import './App.css';
 import { useState } from 'react'
 
 function App() {
+  // [color, setColor] = useState('red')
+  const [color, setColor] = useState(JSON.parse(localStorage.getItem('hex')))
+
   
-  const [color, setColor] = useState('red')
-
-
   function createHex(){    
     const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
-    let hex = '"#';
+    let hex = '#';
     for(let i = 0; i < 6; i++){
     const index = Math.floor(Math.random() * hexValues.length)
     hex += hexValues[index];
     }
-    return hex+'"'  
+    localStorage.setItem('hex', JSON.stringify(hex));
+    return hex 
   }
 
     return (
       <div className="App">
-        <div className='colorBox' style={{background: {color}}} onClick={()=>{setColor(createHex)}}></div>
+        <div className='colorBox' style={{background: color}} onClick={()=>{setColor(createHex)}}></div>
       </div>
     );
+    
 }
 
 
